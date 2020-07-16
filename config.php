160c21194b602cb30786a1b5ca53e5ea7a9f4ee4
petrepan <peterawotola@gmail.com>
2020-07-16 16:02:52 +0100

    commit

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
     //create connection
-    $host = 'localhost';
-    $user = 'b0437c18462dab';
-    $pass = '1feb0731';
-    $db = 'newsletter';
+   $url = parse_url(getenv("mysql://b0437c18462dab:1feb0731@us-cdbr-east-02.cleardb.com/heroku_b4005582b15fe80?reconnect=true"));
 
-    $conn = mysqli_connect($host, $user, $pass, $db);
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = mysqli_connect($server, $username, $password, $db);
 
     //check connection
     if(mysqli_connect_errno()){
